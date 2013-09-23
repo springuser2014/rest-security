5edb31d4c0fe0b4afa02b7942d56bc4870718ffc
lionzzh <lionzzh@gmail.com>
2013-09-23 16:11:48 +0800

    test

--- src/main/java/net/zzh/sec/web/controller/RootController.java
+++ src/main/java/net/zzh/sec/web/controller/RootController.java
@@ -8,6 +8,7 @@ import net.zzh.sec.web.common.UriMappingConstants;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -27,20 +28,31 @@ public class RootController {
 	// search
 
 	@RequestMapping(value = WebConstants.PATH_SEP, method = RequestMethod.GET)
+	public String loadView(String viewName, Model model) {
+		System.out.println("to all page - " + viewName);
+
+		model.addAttribute("title", "This is a good day!");
+		model.addAttribute("theme", "Skyrim");
+
+		return "index";
+	}
+	/*
+	@RequestMapping(value = WebConstants.PATH_SEP, method = RequestMethod.GET)
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void rootWithPathSeparator(final UriComponentsBuilder uriBuilder, final HttpServletResponse response) {
+		System.out.println("rootWithPathSeparator");
 		rootInternal(uriBuilder, response);
 	}
 
 	@RequestMapping(method = RequestMethod.GET)
 	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void rootBare(final UriComponentsBuilder uriBuilder, final HttpServletResponse response) {
+		System.out.println("rootBare");
 		rootInternal(uriBuilder, response);
 	}
 
 	@SuppressWarnings("unused")
 	private void rootInternal(final UriComponentsBuilder uriBuilder, final HttpServletResponse response) {
-		System.out.println("rootInternal");
 		final String userUriNew = uriBuilder.path("/" + UriMappingConstants.USERS).build().toUriString();
 
 		final String userUri = LinkUtil.createLinkHeader(WebConstants.PATH_SEP + UriMappingConstants.USERS, LinkUtil.REL_COLLECTION);
@@ -50,5 +62,5 @@ public class RootController {
 		response.addHeader(HttpHeaders.LINK, discoveredOperations);
 
 	}
-
+*/
 }
--- src/main/webapp/login.jsp
+++ src/main/webapp/login.jsp
@@ -3,9 +3,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>${title}</title>
 </head>
 <body>
-Login page
+<p>Login: ${msg}</p>
 </body>
 </html>
